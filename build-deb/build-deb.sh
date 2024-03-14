15d66a0baa2b0db5497331dd6fc778eeaf518281
phanikumar <phanikumarp@users.noreply.github.com>
2024-03-14 12:58:11 +0530

    Update build-deb.sh
    fixed the jar path issue

--- build-deb/build-deb.sh
+++ build-deb/build-deb.sh
@@ -2,16 +2,19 @@
 
 # Build
 
+BASEDIR=$(dirname "$0")
+echo "../$BASEDIR"
+
 # Workspace: issue-generator git repository local
-cp -v ../target/issuegen-0.1.jar issue-gen/opt/apps/issue-gen/issuegen-0.1.jar
+cp -v ../$BASEDIR/target/issuegen-0.1.jar issue-gen/opt/apps/issue-gen/issuegen-0.1.jar
 
 # Copy scripts to package
-cp -rv ../scripts/ issue-gen/opt/apps/issue-gen/
+cp -rv ../$BASEDIR/scripts/ issue-gen/opt/apps/issue-gen/
 chmod +x issue-gen/opt/apps/issue-gen/scripts/*.sh
 chmod +x issue-gen/opt/apps/issue-gen/scripts/init.d/*.sh
 
 # Package
-pwd
+#pwd
 cp -v issue-gen/DEBIAN/preinst issue-gen/opt/apps/issue-gen/preinst.sh
 cp -v issue-gen/DEBIAN/postinst issue-gen/opt/apps/issue-gen/postinst.sh
 
@@ -19,4 +22,6 @@ chmod +x issue-gen/DEBIAN/preinst
 chmod +x issue-gen/DEBIAN/postinst
 chmod +x issue-gen/opt/apps/issue-gen/*.sh
 
+echo "Started deb to install ...."
 dpkg-deb --build issue-gen
+echo "Installation Completed ......."

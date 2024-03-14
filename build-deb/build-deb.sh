347c7c8fdfedf6f49554afde6cfebad415f51f40
phanikumar <phanikumarp@users.noreply.github.com>
2024-03-14 13:20:16 +0530

    Update build-deb.sh

--- build-deb/build-deb.sh
+++ build-deb/build-deb.sh
@@ -7,7 +7,7 @@ cd -
 cp -v target/issuegen-0.1.jar issue-gen/opt/apps/issue-gen/issuegen-0.1.jar
 
 # Copy scripts to package
-cp -rv scripts/ issue-gen/opt/apps/issue-gen/
+cp -rv -f  scripts/ issue-gen/opt/apps/issue-gen/
 chmod +x issue-gen/opt/apps/issue-gen/scripts/*.sh
 chmod +x issue-gen/opt/apps/issue-gen/scripts/init.d/*.sh
 

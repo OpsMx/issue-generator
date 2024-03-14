e89989448c87f65e7bbceef62d6f20ea60a47961
phanikumar <phanikumarp@users.noreply.github.com>
2024-03-14 13:26:47 +0530

    Update build-deb.sh

--- build-deb/build-deb.sh
+++ build-deb/build-deb.sh
@@ -4,22 +4,22 @@ pwd
 cd -
 
 # Workspace: issue-generator git repository local
-cp -v target/issuegen-0.1.jar issue-gen/opt/apps/issue-gen/issuegen-0.1.jar
+cp -v target/issuegen-0.1.jar $BASEDIR/issue-gen/opt/apps/issue-gen/issuegen-0.1.jar
 
 echo "copy test ........"
 # Copy scripts to package
 cp -rv -f  scripts/  $BASEDIR/issue-gen/opt/apps/issue-gen/
-chmod +x issue-gen/opt/apps/issue-gen/scripts/*.sh
-chmod +x issue-gen/opt/apps/issue-gen/scripts/init.d/*.sh
+chmod +x $BASEDIR/issue-gen/opt/apps/issue-gen/scripts/*.sh
+chmod +x $BASEDIR/issue-gen/opt/apps/issue-gen/scripts/init.d/*.sh
 
 # Package
 #pwd
-cp -v issue-gen/DEBIAN/preinst issue-gen/opt/apps/issue-gen/preinst.sh
-cp -v issue-gen/DEBIAN/postinst issue-gen/opt/apps/issue-gen/postinst.sh
+cp -v $BASEDIR/issue-gen/DEBIAN/preinst issue-gen/opt/apps/issue-gen/preinst.sh
+cp -v $BASEDIR/issue-gen/DEBIAN/postinst issue-gen/opt/apps/issue-gen/postinst.sh
 
-chmod +x issue-gen/DEBIAN/preinst
-chmod +x issue-gen/DEBIAN/postinst
-chmod +x issue-gen/opt/apps/issue-gen/*.sh
+chmod +x $BASEDIR/issue-gen/DEBIAN/preinst
+chmod +x $BASEDIR/issue-gen/DEBIAN/postinst
+chmod +x $BASEDIR/issue-gen/opt/apps/issue-gen/*.sh
 
 echo "Started deb to install ...."
 dpkg-deb --build issue-gen

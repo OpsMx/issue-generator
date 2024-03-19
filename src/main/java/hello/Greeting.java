bf53af872aa67cd4af0b57cd8a3fb49c4c1f98dd
Sagayaraj David <54359431+sagayd@users.noreply.github.com>
2024-03-19 16:02:38 +0530

    Update Greeting.java

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -8,7 +8,7 @@ public class Greeting {
 
     public Greeting(long id, String content) {  
         this.id = id;
-       // this.content = content; Test
+       // this.content = content; Test1
         this.content = content.toString();
     }
 

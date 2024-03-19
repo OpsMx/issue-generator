a950ca6610cf01a42865f7fdb91a5d44b38169b0
phanikumar <phanikumarp@users.noreply.github.com>
2024-03-19 11:43:08 +0530

    Update build-deb.sh
    naming convention changed

--- build-deb/build-deb.sh
+++ build-deb/build-deb.sh
@@ -1,26 +1,26 @@
+#!/bin/bash
+
+# Define variables
 BASEDIR=$(dirname "$0")
-echo "../$BASEDIR"
-pwd
-cd -
+PKG_NAME="issuegen"
+PKG_VERSION="0.1"
+PKG_DIR="$BASEDIR/issue-gen"
+PKG_DEBIAN_DIR="$PKG_DIR/DEBIAN"
+PKG_OPT_DIR="$PKG_DIR/opt/apps/issue-gen"
 
-# Workspace: issue-generator git repository local
-cp -v target/issuegen-0.1.jar $BASEDIR/issue-gen/opt/apps/issue-gen/issuegen-0.1.jar
+# Create directories if they don't exist
+mkdir -p "$PKG_OPT_DIR/scripts/init.d"
 
-# Copy scripts to package
-cp -rv -f  scripts/  $BASEDIR/issue-gen/opt/apps/issue-gen/
-chmod +x $BASEDIR/issue-gen/opt/apps/issue-gen/scripts/*.sh
-chmod +x $BASEDIR/issue-gen/opt/apps/issue-gen/scripts/init.d/*.sh
+# Copy files to package directories
+cp -v "target/issuegen-$PKG_VERSION.jar" "$PKG_OPT_DIR/issuegen-$PKG_VERSION.jar"
+cp -rvf scripts/* "$PKG_OPT_DIR/scripts/"
+cp -v DEBIAN/preinst "$PKG_OPT_DIR/preinst.sh"
+cp -v DEBIAN/postinst "$PKG_OPT_DIR/postinst.sh"
 
-# Package
-#pwd
-cp -rv $BASEDIR/issue-gen/DEBIAN/preinst  $BASEDIR/issue-gen/opt/apps/issue-gen/preinst.sh
-cp -rv $BASEDIR/issue-gen/DEBIAN/postinst $BASEDIR/issue-gen/opt/apps/issue-gen/postinst.sh
+# Set permissions
+chmod +x "$PKG_OPT_DIR/scripts/"*.sh "$PKG_OPT_DIR/scripts/init.d/"*.sh "$PKG_DEBIAN_DIR/"*.sh
 
-chmod +x $BASEDIR/issue-gen/DEBIAN/preinst
-chmod +x $BASEDIR/issue-gen/DEBIAN/postinst
-chmod +x $BASEDIR/issue-gen/opt/apps/issue-gen/*.sh
-
-echo "Prepating the DEBIAN  ...."
-#dpkg-deb --build $BASEDIR/issue-gen
-dpkg-deb --build $BASEDIR/issuegen_0.1_all
-echo "Now DEBIAN Ready"
+# Build Debian package
+echo "Preparing the DEBIAN package..."
+dpkg-deb --build "$PKG_DIR" "$BASEDIR/${PKG_NAME}_${PKG_VERSION}_all.deb"
+echo "DEBIAN package is ready: ${PKG_NAME}_${PKG_VERSION}_all.deb"
